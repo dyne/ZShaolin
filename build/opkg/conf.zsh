dd0e083d4282331e5f5f357b1139ccc9c3a75404
Jaromil <jaromil@dyne.org>
2013-11-17 13:51:36 +0100

    cleanup old ipkg and opkg

--- build/opkg/conf.zsh
+++
@@ -1,9 +0,0 @@
-# ZShaolin build script
-# (C) 2012 Denis Roio - GNU GPL v3
-# refer to zmake for license details
-
-REPO="http://files.dyne.org/zshaolin/packages"
-prepare_sources
-
-notice "Installing packages in sysroot..."
-rsync -ra extract/system/* ${PREFIX}/
