9e4857a8fae42eb1e476a9a6ea26df172bf8e87e
Jaromil <jaromil@dyne.org>
2014-12-21 23:52:28 +0100

    version updates for release

--- termapk/src/org/dyne/zshaolin/startup/installer.java
+++ termapk/src/org/dyne/zshaolin/startup/installer.java
@@ -30,8 +30,8 @@ import org.dyne.zshaolin.startup.setup.filemanager;
 public class installer extends Activity implements OnClickListener{
 
     //THE MAIN INSTALL VALUE
-    public static int      CURRENT_INSTALL_SYSTEM_NUM  = 15;
-    public static String   CURRENT_INSTALL_SYSTEM      = "System v1.0";
+    public static int      CURRENT_INSTALL_SYSTEM_NUM  = 18;
+    public static String   CURRENT_INSTALL_SYSTEM      = "System v1.1";
     public static String   CURRENT_INSTALL_ASSETFILE   = "system.tar.lzma.mp3";
 
     private ProgressDialog mInstallProgress;
